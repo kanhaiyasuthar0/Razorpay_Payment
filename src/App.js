928389c6df90a8c33ea830467f233654d41e0f60
Kanhaiya Suthar <kanhaiyasuthar0@gmail.com>
2022-06-19 14:16:31 +0530

    Payment method implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
 function App() {
+  const [amount, setAmount] = useState(0);
+
+  function loadRazorpay() {
+    const script = document.createElement("script");
+    script.src = "https://checkout.razorpay.com/v1/checkout.js";
+
+    script.onerror = () => {
+      alert("Razorpay load failed");
+    };
+
+    script.onload = async () => {
+      let val = amount;
+      try {
+        // console.log(val);
+        // console.log(amount);
+        const result = await axios.post("http://localhost:5000/create-order", {
+          amount: val + "00",
+        });
+
+        const { amount, id: order_id, currency } = result.data;
+
+        const options = {
+          key: "rzp_test_rEPq0VmasV0z3t",
+          amount: amount.toString(),
+          currency: currency,
+          name: "kanhaiya",
+          description: "expalme desc",
+          order_id: order_id,
+          handler: async function (response) {
+            console.log("response", response);
+            const result = await axios.post("http://localhost:5000/pay-order", {
+              amount: amount,
+              razorpayPaymendId: response.razorpay_payment_id,
+              razorpayOrderId: response.razorpay_order_id,
+              razorpaySignature: response.razorpay_signature,
+            });
+            alert("Payment is successfull");
+          },
+          prefill: {
+            name: "Kanhaiya",
+            email: "emial@email.com",
+            contact: "8976879231",
+          },
+        };
+        const paymentObject = new window.Razorpay(options);
+        paymentObject.open();
+      } catch (error) {
+        console.log("error occured");
+      }
+    };
+    // console.log("completed");
+    document.body.appendChild(script);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="number"
+        placeholder="Amount in INR"
+        onChange={(e) => setAmount(e.target.value)}
+        value={amount}
+      />
+      <button onClick={loadRazorpay}>CheckOut</button>
+      {/* <button onClick={handle}>submit</button> */}
     </div>
   );
 }
